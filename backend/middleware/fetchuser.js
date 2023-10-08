51da0d6e18abc06680f4d263921b03a57a9e7077
Hassan Mushtaq <hhmushtaq@owu.edu>
2023-10-08 00:01:48 -0400

    fixed bug where fetchuser would crash teh server if token was invalid

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -10,16 +10,21 @@ const fetchuser = (req, res, next) => {
             // if an error
             return res.status(401).json({ success, error: "Invalid token." });
         }
+        let errFound = false;
         // verify the token with the secret
         jwt.verify(token, jwtSecret, (err, data) => {
             if (err) {
                 // if an error
+                errFound = true;
                 return res.status(401).json({ success, error: "Invalid token." });
             } else {
                 // add it to the request
                 req.user = data.user;
             }
         })
+        if (errFound) {
+            return;
+        }
         next();
     } catch (error) {
         return res.status(500).json({ error: "Internal server error." });
